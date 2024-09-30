687d44ded59fcae98b8a7b8e54d2df1cad9fbd26
이종수 <whdtnxd@gmail.com>
2024-09-30 15:16:08 +0900

    :fire: 이종수: 필요하지 않은 예시용 테스트 케이스 파일 삭제

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from app.routes.modules.register_routes import register_routes
-from app.utils.generator.bp_generator import generate_blueprint
-from app.routes import main_bp
 from app.utils.blueprint.bp_list import bp_list
 
 
@@ -23,6 +21,6 @@ def create_app():
     CORS(app)
 
     # 라우트 블루프린트 등록
-    register_routes(app, bp_list)  # main과 image_routes를 함께 등록
+    register_routes(app, *bp_list.values())
 
     return app
